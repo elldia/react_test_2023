a98d942c886322aaa8a20d1b000b4aaf1372372b
Hwang <anakyn@naver.com>
2023-07-15 17:46:27 +0900

    ref

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import { useState } from "react";
+import {useState} from 'react';
+
 
 function App() {
-  const [state, setState] = useState({
-    name:'',
-    gender:'',
-    birth:'',
-    bio:''
-  })
-  const handle=(e)=>{
-    console.log('현재대상:', e.target.name);
-    console.log('수정값:',e.target.value);
-    setState({
-      ...state, /* 기존상태를 유지하면서 변경된 값을 정상적으로 업데이트하는데 필요 */
-      [e.target.name]: e.target.value,/*[속성]:값*/
-      
-    })
-  }
+  const [text, setText] = useState('');
+  const handleOnChange = (e)=>{setText(e.target.value);}
+  const handleOnClick =(e)=> {alert(text);}
   return (
     <div>
-      <div className="top">
-        <input type="text" name="name" value={state.name} onChange={handle} placeholder="이름" />
-      </div>
-      <div className="center">
-        <select name="gender" value={state.gender} onChange={handle} id="">
-          <option key={""}></option>
-          <option key={"남성"}>남성</option>
-          <option key={"여성"}>여성</option>
-        </select>
-      </div>
-      <div className="bottom">
-        <input type="date" name="birth" value={state.birth} onChange={handle} id="" />
-      </div>
-      <div className="area">
-        <textarea name="bio" value={state.bio} onChange={handle} cols="30" rows="10"></textarea>
-      </div>
+      <input value={text} onChange={handleOnChange} type="text" />
+      <button onClick={handleOnClick}>작성 완료</button>
     </div>
   );
 }
